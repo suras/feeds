5f096721c1a585db2326b94ed4cce07f0661e07a
V1n0d <fast2fortune@gmail.com>
2013-10-16 17:06:42 +0530

    remove comments

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -4,27 +4,15 @@ class FeedsController < ApplicationController
   	@feeds = @allfeeds.group_by{ |feed| feed.date.strftime("%Y-%m-%d")}
   	@feed_dates = @allfeeds.map{ |feed| feed.date.strftime("%Y-%m-%d")}.uniq
   	@feed_streams = Array.new
-  	# @feed_dates = @feeds.map{ |feed| feed.date }.uniq
-  	# @feed_streams = Hash.new
-  	# @feed_dates.each do |d|
-
-   #    feed = Feed.where(:date => d).order("time desc")
-
-   #    @feed_streams = feed.to_json
-
-  	# end
     @feed_dates.each do |d|
-     ids = @feeds[d].map{|feed| feed.id}
-     #@feed_streams << Feed.select([:stock, :date, :time]).where(:id => ids).count(:group=>"stock")
-     @feeds_in_dates = Feed.where(:id => ids)
-     @feeds_in_dates = @feeds_in_dates.map{|feed| feed.time}.uniq
-     @feeds_in_dates.each do |t|
-       @feed_streams << Feed.where(:id => ids, :time => t).count(:group=>"stock")
-       @feed_streams[@feed_streams.length-1].merge!("date" => d).merge!("time" => t.strftime("%H:%M"))
-     end
-    
+	   ids = @feeds[d].map{|feed| feed.id}
+	   @feeds_in_dates = Feed.where(:id => ids)
+	   @feeds_in_dates = @feeds_in_dates.map{|feed| feed.time}.uniq
+	   @feeds_in_dates.each do |t|
+	      @feed_streams << Feed.where(:id => ids, :time => t).count(:group=>"stock")
+	      @feed_streams[@feed_streams.length-1].merge!("date" => d).merge!("time" => t.strftime("%H:%M"))
+	     end
     end
-
      respond_to do |format|
         format.js
         format.json{render :json => @feed_streams }
--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,24 +1,8 @@
-#require 'open-uri'
-# require 'net/ping'
 class Feed < ActiveRecord::Base
   attr_accessible :date, :status, :stock, :time
   after_save :push_faye
-
-
-
   def push_faye
-
-     # `ping -q -c #{1} #{"http://localhost:3000/feeds.js"}`
-     #Net::HTTP.new("http://localhost:3000/feeds.js").head('/').kind_of? Net::HTTPOK
-     # Net::HTTP.get(URI.parse("http://localhost:3000/feeds.js"))
-     #contents = open("http://localhost:3000/feeds.js").read
-     ping_count = 10
-     server = "http://localhost:3000/feeds.js"
-    #result = `ping -q -c #{ping_count} #{server}`
-    #Net::PingExternal.new(server).ping
-   Net::Ping::HTTP.new(server).ping
-    
-
-
+    server = "http://localhost:3000/feeds.js"
+    Net::Ping::HTTP.new(server).ping
   end
 end
